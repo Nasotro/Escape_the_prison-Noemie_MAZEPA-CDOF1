7d9402131aebb994fbea648210b2fbcca8b19981
Noemiemz <noemie.mazepa@edu.devinci.fr>
2025-01-13 13:59:22 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+name='Adventure_Game-Noemie_MAZEPA-CDOF1',
+version='1.0',
+author='Noemiemz',
+license='MIT',
+long_description=open('README.md').read(),
+)
